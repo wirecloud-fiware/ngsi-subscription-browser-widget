689e216793ef00650a69e58705b7a5165687381c
Carlos Blanco <cblanco@ficodes.com>
2020-03-04 16:55:34 +0100

    Add new editor widget reference by preference

--- src/doc/changelog.md
+++ src/doc/changelog.md
@@ -3,6 +3,7 @@
 - New Reload Endpoint
 - New add subscription button
 - Change the position where the JSON editor widget opens
+- New preference 'widget_editor'. Choose your editor widget for subscriptions.
 
 ## v1.0.2 (2017-12-07)
 
--- src/js/main.js
+++ src/js/main.js
@@ -156,7 +156,11 @@
 
     var openEditorWidget = function openEditorWidget(button, action) {
         if (this.editor_widget == null) {
-            this.editor_widget = mp.mashup.addWidget("CoNWeT/json-editor/1.0.1", {refposition: button});
+            let editor = mp.prefs.get('widget_editor');
+            if (!editor) {
+                editor = "CoNWeT/json-editor/1.0.1";
+            }
+            this.editor_widget = mp.mashup.addWidget(editor, {refposition: button});
             this.editor_widget.addEventListener('remove', onEditorWidgetClose.bind(this));
             // Crete a wiring connection for sending editor conf and initial contents
             this.editor_config_output.connect(this.editor_widget.inputs.configure);
